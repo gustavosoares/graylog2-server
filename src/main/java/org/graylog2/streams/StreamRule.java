cc487f75541a653fbea3fc7c8402b8da97859c4f
Gustavo Soares Souza <gustavosouza@corp.globo.com>
2011-09-19 10:59:45 -0300

    streamrule new type from develop branch

--- src/main/java/org/graylog2/streams/StreamRule.java
+++ src/main/java/org/graylog2/streams/StreamRule.java
@@ -37,6 +37,8 @@ public class StreamRule {
     public final static int TYPE_SEVERITY = 3;
     public final static int TYPE_FACILITY = 4;
     public final static int TYPE_ADDITIONAL = 6;
+    public final static int TYPE_HOSTGROUP = 7;
+    public final static int TYPE_SEVERITY_OR_HIGHER = 8;
 
     private ObjectId objectId = null;
     private int ruleType = 0;
